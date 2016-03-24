f08520ce0063f285782ebad4030b559d306558b3
Obtuse <theobtuseautodidact@gmail.com>
2016-03-24 08:56:16 -0600

    Add store item show action and view

--- app/controllers/stores/items_controller.rb
+++ app/controllers/stores/items_controller.rb
@@ -3,4 +3,8 @@ class Stores::ItemsController < Stores::StoresController
   def index
     @items = current_store.items
   end
+
+  def show
+    @item = current_store.items.find_by(id: params[:id])
+  end
 end
