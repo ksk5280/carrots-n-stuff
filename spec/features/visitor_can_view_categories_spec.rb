18e4737059245c4fba4c507dc35868fe6a6cbe66
Kimiko Kano <kskano@gmail.com>
2016-03-27 22:40:45 -0600

    Add category show and index functionality

--- spec/features/visitor_can_view_categories_spec.rb
+++ spec/features/visitor_can_view_categories_spec.rb
@@ -2,7 +2,22 @@ require "rails_helper"
 
 RSpec.feature "Visitor visits categories page" do
   scenario "sees all categories" do
+    create_roles
+    user = store_admin
+    create_categories
+    store = approved_store(user)
+    item = item3(store)
 
-    
+    visit root_path
+    click_on "Categories"
+    expect(current_path).to eq(categories_path)
+    expect(page).to have_content("Fruits")
+
+    click_on "Fruits"
+    expect(current_path).to eq("/categories/Fruits")
+    expect(page).to have_content "Fruits"
+    expect(page).to have_content "#{item.title}"
+    expect(page).to have_content "$8.00"
+    expect(page).to have_button "Add to Cart"
   end
 end
