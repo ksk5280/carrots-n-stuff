b803dabecf2aca65fb2d27903c74307c9383d9a1
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-03-30 14:14:42 -0600

    Add permissions to store manager
    * Added store manager role permissions to the list
    * added validations on user for email, address, first and last name
    * adjusted factory girl to pass validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :username }
   it { should validate_uniqueness_of :username }
   it { should validate_presence_of :password }
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :address }
+  it { should validate_presence_of :email }
   it { should have_secure_password }
   it { should have_many :orders }
   it { should belong_to :store }
